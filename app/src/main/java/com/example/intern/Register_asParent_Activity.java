6d4e714d5ba123221b88ac3a674c4702a2e94791
SwapneelJanrao <swapneel.j@ideastarz.com>
2020-03-08 18:43:26 +0530

    first 4 Screens 1)Login Register 2)Welcome 3)Login As Parent Activity 4)Login As Child Activity

---
+++ app/src/main/java/com/example/intern/Register_asParent_Activity.java
@@ -0,0 +1,16 @@
+package com.example.intern;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register_asParent_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_as_parent);
+        getSupportActionBar().hide();
+
+    }
+}
