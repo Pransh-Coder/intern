5347b64c94cddc1e286e3654c8ad506642065cdb
harshchaplot <39793774+harshchaplot@users.noreply.github.com>
2020-03-23 00:52:44 +0530

    lifestyle_list_frontend

--- app/src/main/java/com/example/intern/MyAdapter_lifestyle.java
+++ app/src/main/java/com/example/intern/MyAdapter_lifestyle.java
@@ -1,7 +1,7 @@
 package com.example.intern;
+
 import android.content.Context;
 import android.text.Html;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,24 +13,24 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MyAdapter_food extends RecyclerView.Adapter<MyAdapter_food.ViewHolder> {
+public class MyAdapter_lifestyle extends RecyclerView.Adapter<MyAdapter_lifestyle.ViewHolder> {
     private String [] param;
     private Context context;
 
-    public MyAdapter_food( Context context,String [] param ){
+    public MyAdapter_lifestyle( Context context,String [] param ){
         this.param = param;
         this.context = context;
     }
     @NonNull
     @Override
-    public MyAdapter_food.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.recyclerview_food,parent,false);
+    public MyAdapter_lifestyle.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.recyclerview_lifestyle,parent,false);
         ViewHolder viewHolder = new ViewHolder(view);
         return viewHolder;
     }
     @Override
-    public void onBindViewHolder(@NonNull MyAdapter_food.ViewHolder holder, int position) {
-        holder.recycle_food.setText(Html.fromHtml(param[position]));
+    public void onBindViewHolder(@NonNull MyAdapter_lifestyle.ViewHolder holder, int position) {
+        holder.recycle_lifestyle.setText(Html.fromHtml(param[position]));
     }
     @Override
     public int getItemCount() {
@@ -38,17 +38,17 @@ public class MyAdapter_food extends RecyclerView.Adapter<MyAdapter_food.ViewHold
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView recycle_food;
+        TextView recycle_lifestyle;
         CheckBox cb;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            recycle_food = itemView.findViewById(R.id.tv_recycle_food);
+            recycle_lifestyle = itemView.findViewById(R.id.tv_recycle_lifestyle);
             cb = itemView.findViewById(R.id.checkBox);
             cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
                 public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                     if(cb.isChecked()) {
-                        String str = recycle_food.getText().toString();
+                        String str = recycle_lifestyle.getText().toString();
                         int index = str.indexOf('\n');
                         str = str.substring(0,index);
                         Toast.makeText(context, str,Toast.LENGTH_LONG).show();
