d11daa1956c10467445ee2aef0aa12deb0a8a60f
codejack-CR <chandanrajsingh19@gmail.com>
2020-03-24 00:02:56 +0530

    Integrated user side QR scanner

--- app/src/main/java/com/example/intern/qr/QRScanner.java
+++ app/src/main/java/com/example/intern/qr/QRScanner.java
@@ -1,45 +1,71 @@
 package com.example.intern.qr;
 
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.intern.databinding.ActivityQrBinding;
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.android.BeepManager;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.DefaultDecoderFactory;
 
 import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 public class QRScanner extends AppCompatActivity {
 	private static String TAG = QRScanner.class.getSimpleName();
 	private ActivityQrBinding binding;
-	
+	private String lastText;
+	private BeepManager beepManager;
+	private BarcodeCallback callback = new BarcodeCallback() {
+		@Override
+		public void barcodeResult(BarcodeResult result) {
+			//Check for duplicate
+			if(result.getText() == null)return;
+			//update content
+			binding.qrView.pauseAndWait();
+			lastText = result.getText();
+			beepManager.playBeepSoundAndVibrate();
+			binding.ivScannedImage.setImageBitmap(result.getBitmapWithResultPoints(Color.YELLOW));
+			showImageAndPopulate(result);
+		}
+	};
 	@Override
 	public void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		binding = ActivityQrBinding.inflate(getLayoutInflater());
 		setContentView(binding.getRoot());
+		Collection<BarcodeFormat> formats = Arrays.asList(BarcodeFormat.QR_CODE );
+		binding.qrView.getBarcodeView().setDecoderFactory( new DefaultDecoderFactory(formats));
+		binding.qrView.initializeFromIntent(getIntent());
+		binding.qrView.setStatusText("Scan a QR code from the vendor");
 	}
 	
 	@Override
 	protected void onResume() {
 		super.onResume();
+		binding.qrView.resume();
 		binding.button.setOnClickListener(v->{
-			IntentIntegrator integrator = new IntentIntegrator(this);
-			integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
-			integrator.setPrompt("Scan the vendor QR code");
-			integrator.setBeepEnabled(true);
-			integrator.setBarcodeImageEnabled(true);
-			integrator.setTorchEnabled(true);
-			integrator.initiateScan();
+			binding.qrView.resume();
+			binding.ivScannedImage.setVisibility(View.INVISIBLE);
+			binding.qrView.setVisibility(View.VISIBLE);
+			binding.qrView.decodeSingle(callback);
+			beepManager = new BeepManager(this);
+			beepManager.setVibrateEnabled(true);
+			beepManager.setBeepEnabled(true);
 		});
+		//TODO: confirm that user has taken offer using the redeem button
 	}
 	
-	@Override
+/*	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
 		if(result != null) {
@@ -53,15 +79,31 @@ public class QRScanner extends AppCompatActivity {
 		} else {
 			super.onActivityResult(requestCode, resultCode, data);
 		}
+	}*/
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		binding.qrView.pause();
 	}
 	
-	private void getQRData(IntentResult result){
+	private void showImageAndPopulate(BarcodeResult result){
+		binding.ivScannedImage.setVisibility(View.VISIBLE);
+		binding.qrView.setVisibility(View.INVISIBLE);
+		binding.textView.setText(getQRData(lastText));
+	}
+	
+	private String getQRData(String result){
 		try{
-			JSONObject data = new JSONObject(result.getContents());
+			JSONObject data = new JSONObject(result);
+			if(data.has("offer")){
+				binding.qrView.setStatusText("Found an offer!");
+			}
 			String vendorData = data.get("vendor") + "\n" + data.get("offer");
-			binding.textView.setText(vendorData);
+			return vendorData;
 		}catch (Exception e){
 			Log.d(TAG, "getQRData: JSON parse error");
 		}
+		return "No Such Vendor Found";
 	}
 }
