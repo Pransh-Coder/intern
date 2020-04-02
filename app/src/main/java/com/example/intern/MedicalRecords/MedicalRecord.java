108941bb58c8cc076e8093bfe0f2bf35bdde7bf4
Pransh-Coder <rastogipransh4@gmail.com>
2020-04-02 17:46:32 +0530

    My Screens done but some changes nedded!!

---
+++ app/src/main/java/com/example/intern/MedicalRecords/MedicalRecord.java
@@ -0,0 +1,16 @@
+package com.example.intern.MedicalRecords;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.intern.R;
+
+public class MedicalRecord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medical_record);
+    }
+}
---
+++ app/src/main/java/com/example/intern/Vitals/Vitals.java
@@ -0,0 +1,16 @@
+package com.example.intern.Vitals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.intern.R;
+
+public class Vitals extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vitals);
+    }
+}
