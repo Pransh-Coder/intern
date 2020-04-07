643bcc4f43596e9cb3e041cfde913b4a73c07c53
harshchaplot <39793774+harshchaplot@users.noreply.github.com>
2020-04-08 00:16:08 +0530

    update

---
+++ app/src/main/java/com/example/intern/login_01_04.java
@@ -0,0 +1,15 @@
+package com.example.intern;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login_01_04 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_01_04);
+        getSupportActionBar().hide();
+    }
+}
