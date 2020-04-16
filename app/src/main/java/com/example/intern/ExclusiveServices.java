f0f30cfd25bf63602d37118540a0e8488348a27a
SwapneelJanrao <swapneel.j@ideastarz.com>
2020-04-16 11:57:42 +0530

    no changes made

--- app/src/main/java/com/example/intern/ExclusiveServices.java
+++ app/src/main/java/com/example/intern/ExclusiveServices.java
@@ -13,7 +13,7 @@ public class ExclusiveServices extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exclusive_services);
+        setContentView(R.layout.activity_exclusive_service);
 
         back = findViewById(R.id.back);
         home = findViewById(R.id.homeIMG);
