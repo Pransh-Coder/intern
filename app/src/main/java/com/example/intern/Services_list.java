a99332917ce89ed0033cb7a8c9644d3793ac5e89
TechieAditi <55248714+TechieAditi@users.noreply.github.com>
2020-05-06 13:19:43 +0530

    Modifications

--- app/src/main/java/com/example/intern/Services_list.java
+++ app/src/main/java/com/example/intern/Services_list.java
@@ -1,6 +1,9 @@
 package com.example.intern;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -24,5 +27,12 @@ public class Services_list extends AppCompatActivity {
         MyAdapter_services recycleadapter = new MyAdapter_services(this, mname);
         myrecycleview.setAdapter(recycleadapter);
         myrecycleview.setLayoutManager(new LinearLayoutManager(this));
+        ImageView chatButton=findViewById(R.id.chat_button);
+        chatButton.setOnClickListener(v -> {
+            String url = "https://wa.me/919909906065?text=Hi%20PS,%20I%20have%20some%20queries";
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+            startActivity(intent);
+        });
     }
 }
